ce0db8ba03d64a9c275c8c2f684752de17d92dbd
Lovodia <kosokinman@mail.ru>
2025-06-24 21:26:11 +0300

    Task1.3

--- main.go
+++ main.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
-// Изменнеия для PULLREquest
-// ЗАПУСК РАСЧЁТА ЧЕРЕЗ ТЕЛО POST ЗАПРОСА В JSON ФОРМАТЕ (НЕОБХОДИМО ПЕРЕДАТЬ СРЕЗ {"values": [1, 4, 3, 6]})
 type Numbers struct {
-	Values []float64 `json:"values"` // Свзяь с JSON
+	Values []float64 `json:"values"`
 }
+
 type SumResponse struct {
 	Sum float64 `json:"sum"`
 }
 
+// ввод в формате {"values": [1, 6, 3, 6]}
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost { //Поверка на метод запроса (используется только POST запрос)
-		http.Error(w, "Only POST method is supported", http.StatusMethodNotAllowed) // 405
+	if r.Method != http.MethodPost {
+		statusCode := http.StatusMethodNotAllowed
+		responseBody := "Only POST method is supported"
+		log.Printf("Error %d: %s", statusCode, responseBody)
+		http.Error(w, responseBody, statusCode)
 		return
 	}
 
 	var nums Numbers
-	if err := json.NewDecoder(r.Body).Decode(&nums); err != nil { // Декодеривание JSON
-		http.Error(w, "invalid data format", http.StatusBadRequest) //Ошибка при декодировании
+	if err := json.NewDecoder(r.Body).Decode(&nums); err != nil {
+		statusCode := http.StatusBadRequest
+		responseBody := "Invalid data format"
+		log.Printf("Error %d: %s, decode error: %v", statusCode, responseBody, err)
+		http.Error(w, responseBody, statusCode)
 		return
 	}
 
@@ -30,17 +37,24 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	for _, v := range nums.Values {
 		sum += v
 	}
+
 	resp := SumResponse{Sum: sum}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(resp); err != nil { //ошибка при декодировании
-		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		statusCode := http.StatusInternalServerError
+		responseBody := "Failed to encode response"
+		log.Printf("Error %d: %s, encode error: %v", statusCode, responseBody, err)
+		http.Error(w, responseBody, statusCode)
 		return
 	}
 }
 
 func main() {
 	http.HandleFunc("/post", PostHandler)
-	http.ListenAndServe("localhost:8080", nil)
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+
 }
