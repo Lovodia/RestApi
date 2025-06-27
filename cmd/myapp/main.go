85ee08a8c78d931bc8441bcedf4648ed5fd68a26
Lovodia <kosokinman@mail.ru>
2025-06-27 16:38:39 +0300

    Task4.1

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"log/slog"
 	"os"
 
 	"github.com/labstack/echo/v4"
@@ -10,6 +9,7 @@ import (
 
 	"github.com/Lovodia/restapi/internal/handlers"
 	"github.com/Lovodia/restapi/pkg/config"
+	"github.com/Lovodia/restapi/pkg/logger1" // импортируем новый пакет
 )
 
 func main() {
@@ -18,25 +18,7 @@ func main() {
 		log.Fatalf("failed to load config: %v", err)
 	}
 
-	// Инициализация логгера с указанным уровнем
-	var logLevel slog.Level
-	switch cfg.Logger.Level {
-	case "debug":
-		logLevel = slog.LevelDebug
-	case "info":
-		logLevel = slog.LevelInfo
-	case "warn":
-		logLevel = slog.LevelWarn
-	case "error":
-		logLevel = slog.LevelError
-	default:
-		logLevel = slog.LevelInfo
-	}
-
-	opts := &slog.HandlerOptions{
-		Level: logLevel,
-	}
-	logger := slog.New(slog.NewTextHandler(os.Stderr, opts))
+	logger := logger1.NewLogger(cfg.Logger.Level)
 
 	e := echo.New()
 	e.Logger.SetOutput(os.Stdout)
---
+++ pkg/logger1/logger1.go
@@ -0,0 +1,29 @@
+package logger1
+
+import (
+	"log/slog"
+	"os"
+)
+
+// NewLogger создаёт новый slog.Logger с уровнем, заданным строкой levelStr
+func NewLogger(levelStr string) *slog.Logger {
+	var logLevel slog.Level
+	switch levelStr {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	return slog.New(slog.NewTextHandler(os.Stderr, opts))
+}
